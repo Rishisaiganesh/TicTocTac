fe060645d1698be049d674dec136dc27d020d9e9
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-03 13:47:25 +0530

    [Rishi]Adding:UserMove

--- TicTacToc/TicTacToc/Program.cs
+++ TicTacToc/TicTacToc/Program.cs
@@ -7,11 +7,12 @@ namespace TicTacToc
         static void Main(string[] args)
         {
 
-            char[] board = Tic_TacToc .createBoard();                 
+            char[] board = Tic_TacToc.createBoard();                 
             Console.WriteLine(board);
             char choose = Tic_TacToc.chooseUserChar();
             Console.WriteLine("Your choice is " + choose);
             Tic_TacToc.showBoard(board);
+            int userMove = Tic_TacToc.getUserMove(board);
 
             Console.ReadKey();
         }
--- TicTacToc/TicTacToc/Tic-TacToc.cs
+++ TicTacToc/TicTacToc/Tic-TacToc.cs
@@ -16,6 +16,10 @@ namespace TicTacToc
             return board;
         }
 
+        /// <summary>
+        /// Method to take user choice that its O or X
+        /// </summary>
+        /// <returns>returns an user selection</returns>
         public static char chooseUserChar()
         {
             Console.WriteLine("Enter your choice X or O :");
@@ -23,17 +27,37 @@ namespace TicTacToc
             return char.ToUpper(userSelection);
         }
 
-
+        /// <summary>
+        /// Method to show an board 
+        /// </summary>
         public static void showBoard(char[] board)
         {
             Console.WriteLine("\n " + board[1] + " | " + board[2] + " | " + board[3]);
             Console.WriteLine("_________________");
             Console.WriteLine(" " + board[4] + " | " + board[5] + " | " + board[6]);
             Console.WriteLine("_________________");
-            Console.WriteLine(" " + board[7] + " | " + board[8] + " | " + board[9];
+            Console.WriteLine(" " + board[7] + " | " + board[8] + " | " + board[9]);
+        }
 
-
+        /// <summary>
+        /// Getting user move 
+        /// </summary>
+        /// <returns></returns>
+        public static int getUserMove(char[] board)
+        {
+            int[] validCells = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            while (true)
+            {
+                Console.WriteLine("What is Your next move(1-9)???");
+                int index = Convert.ToInt32(Console.ReadLine());
+                if (Array.Find<int>(validCells, elements => elements == index) != 0 && isFreeSpace(board, index))
+                    return index;
+            }
+        }
+        public static bool isFreeSpace(char[] board, int index)
+        {
+            return board[index] == ' ';
         }
     }
-}    
+}
 
