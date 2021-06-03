2437d1d13836b20119057ce445f476a780384049
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-03 13:29:42 +0530

    [Rishi]Adding:tictactocGame

---
+++ TicTacToc/TicTacToc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tic_TacToc tic = new Tic_TacToc();         
+            tic.createBoard();                      
+            char usersChoice = tic.chooseUserChar();
+            Console.WriteLine("Users choice is :" + usersChoice);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TicTacToc/TicTacToc/Tic-TacToc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToc
+{
+    class Tic_TacToc
+    {
+        public char[] createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                Console.WriteLine(board[i] = '_');
+            }
+            return board;
+        }
+        public char chooseUserChar()
+        {
+            Console.WriteLine("Enter your choice X or O :");
+            char userSelection = Convert.ToChar(Console.ReadLine());
+            return char.ToUpper(userSelection);
+        }
+    }
+}
